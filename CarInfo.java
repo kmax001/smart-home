0bdca44ee076aec83abedefe0df9604420b341d1
kmax001 <k_max@126.com>
2018-08-23 08:12:07 +0800

    RFID 表结构，用于实现车库车辆出入功能

---
+++ CarInfo.java
@@ -0,0 +1,12 @@
+package com.android_wifi;
+
+public class CarInfo{
+
+	boolean isUse;             //是否有车停在车库     true:有车停在车库
+	String rfid;			//rfid上报数据
+    String name;          //对应的人名
+
+	public CarInfo(){
+		isUse=false;
+	}
+}
